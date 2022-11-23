ef9241d8f532670eac46e61c35381a331b40dfda
Alexey <ReetzCraft@gmail.com>
2022-11-23 14:26:59 +0300

    loops part 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //second part
+        //task 1
+        int total = 15_000;
+        int monthNumber = 0;
+        while (total < 12_000_000) {
+            total = total + total * 7 / 100;
+            monthNumber++;
+            System.out.println("Месяц " + monthNumber + ", сумма накоплений равна " + total);
+        }
+        System.out.println("Всего месяцев потребовалось " + monthNumber);
+
+        //task 2
+        total = 15_000;
+        monthNumber = 0;
+        while (total < 12_000_000) {
+            total = total + total * 7 / 100;
+            monthNumber++;
+            if (monthNumber % 6 == 0) {
+                System.out.println("Месяц " + monthNumber + ", сумма накоплений равна " + total);
+            }
+        }
+        System.out.println("Всего месяцев потребовалось " + monthNumber);
+
+        //task 3
+        total = 15_000;
+        int years = 9;
+        int monthsTotal = years * 12;
+        monthNumber = 0;
+        while (monthNumber < monthsTotal) {
+            total = total + total * 7 / 100;
+            monthNumber++;
+            if (monthNumber % 6 == 0) {
+                System.out.println("Полугодие " + monthNumber / 6 + " сумма " + total);
+            }
+        }
+        //task 4
+        int firstFridayOfMonth = 4;
+        while (firstFridayOfMonth < 31) {
+            System.out.println("Сегодня пятница, " + firstFridayOfMonth + "-е число. Необходимо подготовить отчет.");
+            firstFridayOfMonth += 7;
+
+        }
     }
 }
